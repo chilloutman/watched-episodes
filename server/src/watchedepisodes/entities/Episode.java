31130501f302851d3e6f9198d1ea2fc29da39ed1
lneiva <lucasneiv4@gmail.com>
2011-01-01 17:32:49 +0100

    Added new entity for episodes.

---
+++ server/src/watchedepisodes/entities/Episode.java
@@ -0,0 +1,120 @@
+package watchedepisodes.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+
+@PersistenceCapable
+public class Episode {
+	@PrimaryKey
+	private String id;
+	@Persistent
+	private Series series;
+	
+	@Persistent
+	private String episodeName;
+	@Persistent
+	private int seasonNumber;
+	@Persistent
+	private int episodeNumber;
+	
+	private String overview;
+	@Persistent
+	private String language;
+	@Persistent
+	private String firstAired;
+	@Persistent
+	private String imdbId;
+	@Persistent
+	private String rating;
+	
+	@Persistent
+	private List<String> writers = new ArrayList<String>();
+	@Persistent
+	private List<String> directors = new ArrayList<String>();
+	@Persistent
+	private List<String> guestStars = new ArrayList<String>();
+	
+	public Series getSeries() {
+		return series;
+	}
+	public void setSeries(Series series) {
+		this.series = series;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEpisodeName() {
+		return episodeName;
+	}
+	public void setEpisodeName(String episodeName) {
+		this.episodeName = episodeName;
+	}
+	public int getSeasonNumber() {
+		return seasonNumber;
+	}
+	public void setSeasonNumber(int seasonNumber) {
+		this.seasonNumber = seasonNumber;
+	}
+	public int getEpisodeNumber() {
+		return episodeNumber;
+	}
+	public void setEpisodeNumber(int episodeNumber) {
+		this.episodeNumber = episodeNumber;
+	}
+	public String getOverview() {
+		return overview;
+	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getFirstAired() {
+		return firstAired;
+	}
+	public void setFirstAired(String firstAired) {
+		this.firstAired = firstAired;
+	}
+	public String getImdbId() {
+		return imdbId;
+	}
+	public void setImdbId(String imdbId) {
+		this.imdbId = imdbId;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public List<String> getWriters() {
+		return writers;
+	}
+	public void setWriters(List<String> writers) {
+		this.writers = writers;
+	}
+	public List<String> getDirectors() {
+		return directors;
+	}
+	public void setDirectors(List<String> directors) {
+		this.directors = directors;
+	}
+	public List<String> getGuestStars() {
+		return guestStars;
+	}
+	public void setGuestStars(List<String> guestStars) {
+		this.guestStars = guestStars;
+	}
+}
--- server/src/watchedepisodes/entities/Series.java
+++ server/src/watchedepisodes/entities/Series.java
@@ -1,6 +1,7 @@
 package watchedepisodes.entities;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
@@ -12,69 +13,61 @@ import com.google.appengine.api.datastore.Text;
 public class Series {
 	@PrimaryKey
 	private String id;
+	@Persistent (mappedBy= "series")
+	private List<Episode> episodes;
 	
 	@Persistent
 	private String name;
-	
 	@Persistent
 	private Text overview;
 	
 	@Persistent
 	private Date firstAired;
-	
 	@Persistent
 	private String banner;
-	
 	@Persistent
 	private String imdbId;
 
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
+	public void setEpisodes(List<Episode> episodes) {
+		this.episodes = episodes;
+	}
+	public List<Episode> getEpisodes() {
+		return episodes;
+	}
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public Text getOverview() {
 		return overview;
 	}
-
 	public void setOverview(Text overview) {
 		this.overview = overview;
 	}
-
 	public Date getFirstAired() {
 		return firstAired;
 	}
-
 	public void setFirstAired(Date firstAired) {
 		this.firstAired = firstAired;
 	}
-
 	public String getBanner() {
 		return banner;
 	}
-
 	public void setBanner(String banner) {
 		this.banner = banner;
 	}
-
 	public String getImdbId() {
 		return imdbId;
 	}
-
 	public void setImdbId(String imdbId) {
 		this.imdbId = imdbId;
 	}
-	
-	
 }
