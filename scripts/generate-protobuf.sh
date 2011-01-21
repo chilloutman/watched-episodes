cb5e2b2f2c733c46cab799093646f7ff0e8de5a8
lneiva <lucasneiv4@gmail.com>
2011-01-21 19:49:56 +0100

    Created proto for getSeries-request.

--- scripts/generate-protobuf.sh
+++ scripts/generate-protobuf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-protoc --proto_path=../shared/protobuf/ --objc_out=../ios-client/Generated/ --java_out=../server/src/ *.proto
+protoc --proto_path=shared/protobuf/ --objc_out=ios-client/Generated/ --java_out=server/src/ shared/protobuf/*.proto
