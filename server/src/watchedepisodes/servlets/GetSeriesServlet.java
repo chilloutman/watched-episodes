ed2db3228efe5c275e30140196b77fc4fd6b3de8
lneiva <lucasneiv4@gmail.com>
2011-01-21 20:13:01 +0100

    Added protocol buffers capabilities to the GetSeries servlet.

--- server/src/watchedepisodes/servlets/GetSeriesServlet.java
+++ server/src/watchedepisodes/servlets/GetSeriesServlet.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import watchedepisodes.entities.Series;
+import watchedepisodes.servlets.protocols.GetSeriesProtocol.PBSeries;
 import watchedepisodes.thetvdb.TVDB;
 import watchedepisodes.thetvdb.TVDBException;
 import watchedepisodes.tools.ServiceLocator;
@@ -17,13 +17,14 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 
 @SuppressWarnings("serial")
-public class GetSeriesServlet extends HttpServlet {
+public class GetSeriesServlet extends AbstractServlet {
 	
 	private String id;
 	private String language;
 	
-	public void doGet (HttpServletRequest request, HttpServletResponse resp) {
-		getParameters(request);
+	public void doGet (HttpServletRequest request, HttpServletResponse response) {
+		id= request.getParameter("id");
+		language= getLanguage(request);
 		
 		PersistenceManager pm= ServiceLocator.getPersistenceManagerFactory().getPersistenceManager();
 		Series series= null;
@@ -36,34 +37,13 @@ public class GetSeriesServlet extends HttpServlet {
 				series= fetchSeriesFromTVDB();
 				pm.makePersistent(series);
 			} catch (TVDBException e1) {
-				// TODO Handle this
+				e1.printStackTrace();
+				response.setStatus(500);
 			}
 		}
 		
 		pm.close();
-		
-		// TODO: Remove debug html stuff
-		resp.setContentType("text/html");
-		try {
-			if (series == null) {
-				resp.getWriter().append("Could not get series with this id:" + id);
-			} else {
-				resp.getWriter().append(getHtml(series));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void getParameters (HttpServletRequest request) {
-		id= request.getParameter("id");
-		if (id == null) {
-			return;
-		}
-		language= request.getParameter("lang");
-		if (language == null) {
-			language= "en";
-		}
+		writeResponse(response, request, series);
 	}
 	
 	private Series fetchSeriesFromTVDB () throws TVDBException {
@@ -71,20 +51,56 @@ public class GetSeriesServlet extends HttpServlet {
 		return tvdb.getSeries(id, language);
 	}
 	
-	private String getHtml(Series s) {
-		StringBuilder html= new StringBuilder();
-		html.append("<h1>Series Info:</h1>");
-		
-		html.append("id: " + s.getId() + "<br />");
-		html.append("name: " + s.getName() + "<br />");
-		for (String a : s.getActors()) {
-			html.append("<t />Actor: " + a  + "<br />");
+	private void writeResponse (HttpServletResponse response, HttpServletRequest request, Series series) {
+		try {
+			if (clientAcceptsProtocolBuffers(request)) {
+				writeProtobuf(response, series);
+			} else {
+				writeHtml(response, series);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			response.setStatus(500);
 		}
-		html.append("first aired: " + s.getFirstAired() + "<br />");
-		html.append("banner: " + s.getBanner() + "<br />");
-		html.append("imdb: " + s.getImdbId() + "<br />");
-		html.append("overview: " + s.getOverview().getValue() + "<br />");
+	}
+	
+	private void writeProtobuf(HttpServletResponse response, Series s) throws IOException {
+		response.setContentType(protocolBuffers);
 		
-		return html.toString();
+		PBSeries.Builder series= PBSeries.newBuilder();
+		series.setId(s.getId());
+		series.setLanguage(s.getLanguage());
+		series.setName(s.getName());
+		series.setOverview(s.getOverview().toString());
+		series.setFirstAired(s.getFirstAired());
+		series.addAllActors(s.getActors());
+		series.setBanner(s.getBanner());
+		series.setImdbId(s.getImdbId());
+		
+		PBSeries protobuf= series.build();
+		response.setBufferSize(protobuf.getSerializedSize());
+		protobuf.writeTo(response.getOutputStream());
+	}
+	
+	private void writeHtml(HttpServletResponse response, Series s) throws IOException {
+		if (s == null) {
+			response.getWriter().append("Could not get series with this id:" + id);
+		} else {
+			StringBuilder html= new StringBuilder();
+			html.append("<h1>Series Info:</h1>");
+			
+			html.append("id: " + s.getId() + "<br />");
+			html.append("name: " + s.getName() + "<br />");
+			for (String a : s.getActors()) {
+				html.append("<t />Actor: " + a  + "<br />");
+			}
+			html.append("first aired: " + s.getFirstAired() + "<br />");
+			html.append("banner: " + s.getBanner() + "<br />");
+			html.append("imdb: " + s.getImdbId() + "<br />");
+			html.append("overview: " + s.getOverview().getValue() + "<br />");
+			
+			response.getWriter().append(html.toString());
+		}
+		
 	}
 }
